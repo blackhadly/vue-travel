3204cde843518e33991d9d16d67d4212513d350a
blackhad <you@example.com>
2019-02-07 18:01:31 +0800

    add icon

---
+++ vue-travel/src/pages/home/components/icons.vue
@@ -0,0 +1,131 @@
+<template>
+    <div class="icons">
+        <swiper :options="swiperOption">
+            <swiper-slide v-for="(page,index) of pages" :key="index">
+                <div class="icon" v-for="item of page" :key="item.id" >
+                    <div class="icon-img">
+                        <img class="icon-img-content" :src="item.imgUrl" alt="">
+                        <p class="icon-desc">{{item.desc}}</p>
+                    </div>
+                </div>
+            </swiper-slide>
+        </swiper>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'icons',
+  data () {
+    return {
+      iconList: [
+        {
+          id: '001',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          desc: '景点门票'
+        },
+        {
+          id: '002',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          desc: '景点门票'
+        },
+        {
+          id: '003',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          desc: '景点门票'
+        },
+        {
+          id: '004',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          desc: '景点门票'
+        },
+        {
+          id: '005',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          desc: '景点门票'
+        },
+        {
+          id: '006',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          desc: '景点门票'
+        },
+        {
+          id: '007',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          desc: '景点门票'
+        },
+        {
+          id: '008',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          desc: '景点门票'
+        },
+        {
+          id: '009',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          desc: '景点门票'
+        }
+      ],
+      swiperOption: {
+        autoplay: false
+      }
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style scoped>
+    .icons >>> .swiper-container {
+        height: 0;
+        padding-bottom: 50%
+    }
+    .icon{
+        position: relative;
+        overflow: hidden;
+        float: left;
+        width: 25%;
+        height: 0;
+        padding-bottom: 25%;
+    }
+    .icon-img{
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 44px;
+        box-sizing: border-box;
+        padding: 10px;
+    }
+
+    .icon-img-content {
+        display: block;
+        margin: 0 auto;
+        height: 100%;
+    }
+
+    .icon-desc {
+        position: absolute;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        height: 44px;
+        line-height: 44px;
+        text-align: center;
+        color: #333;
+        overflow: hidden;
+        white-space: nowrap;
+        text-overflow: ellipsis
+    }
+</style>
--- vue-travel/src/pages/home/home.vue
+++ vue-travel/src/pages/home/home.vue
@@ -2,17 +2,21 @@
     <div>
         <Header></Header>
         <swipper></swipper>
+        <icons></icons>
     </div>
 </template>
 
 <script>
 import Header from './components/Header'
 import swipper from './components/swipper'
+import icons from './components/icons'
+
 export default {
   name: 'Home',
   components: {
     Header,
-    swipper
+    swipper,
+    icons
   }
 }
 </script>
